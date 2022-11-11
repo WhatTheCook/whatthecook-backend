d7a793d6bfd65ab0e69de9f223f3b997fd6485a1
Stamp <pphawikaa@gmail.com>
2022-11-11 23:35:57 +0700

    [ADD] reset password

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,10 @@ const {hashPassword} = require("../util/encrypt");
 const {generateToken, authenticate} = require("../util/jwt");
 const {login} = require("../controller/user");
 const {register} = require("../controller/user");
+const  nanoid =  require("nanoid");
+const nodemailer = require('nodemailer');
+const NodeCache = require( "node-cache" );
+const Cache = new NodeCache();
 
 //get user
 router.get('/', authenticate ,async (req,res) => {
@@ -27,5 +31,37 @@ router.get('/', authenticate ,async (req,res) => {
 router.post('/login', login)
 // register
 router.post('/register',register)
+
+//reset password
+router.post('/resetpassword' ,async (req,res) => {
+    const {email} = req.body;
+    const Currentuser = await user.findFirst({
+        where:{
+            email:email
+        }
+    })
+    if (Currentuser == null) {
+        res.sendStatus(404)
+        return
+    }
+    const OTP = nanoid.customAlphabet("0123456789",6)()
+    Cache.set(OTP,Currentuser.id,300)
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'whatthecook3567@gmail.com',
+            pass: 'yxfwdhjdbokgkfap'
+        }
+    });
+    const mailOptions = {
+        from: 'whatthecook3567@gmail.com', // sender address
+        to: email, // list of receivers
+        subject: 'WhatTheCook reset password', // Subject line
+        html: `<h1>แก้รหัสผ่านค่าอีสัด </h1> ${OTP}`// plain text body
+    };
+    await transporter.sendMail(mailOptions)
+   
+    res.json(OTP)
+})
 
 module.exports = router
